445ff6b5eb811606b3692e64aa05ac39bc29ebdf
Ayoubyte <ayoubboubkrioui2@gmail.com>
2023-08-17 15:11:19 +0100

    Add utils method to manage get and save task
    Utils helps loading and saving data to a json file:
    - load_data helps to read data from a file then convertit to json data, the python structure.
    - save_tasks helps in save task data in data.json file.

---
+++ utils.py
@@ -0,0 +1,26 @@
+import json
+from datetime import datetime
+
+TASKS_FILE = 'data.json' #TODO Add file path later 
+def load_data():
+        try:
+            with open(TASKS_FILE, "r") as f:
+                tasks = json.load(f)
+                return [(task["title"], task["description"])for task in tasks]
+        except FileNotFoundError:
+            return []
+
+def save_tasks(tasks):
+        tasks_data = [{
+             "title": task.title, 
+             "description": task.description, 
+            #  "completedAt": task.completedAt.strftime("%Y-%m-%d %H:%M:%S") if task.completedAt else None} 
+            }for task in tasks] 
+        with open(TASKS_FILE, "w") as f:
+            json.dump(tasks_data, f, indent=4)
+
+# def update_time():
+#     current_date = datetime.now()
+#     formatted_date = current_date.strftime("%Y-%m-%d %H:%M")
+
+#     return formatted_date
